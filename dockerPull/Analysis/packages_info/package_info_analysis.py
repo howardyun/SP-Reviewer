5277e24a38830018c7860157145f34ec8bc6b19b
howardyun <howard.yun@outlook.com>
2025-04-30 16:50:12 +0800

    add package_info_analysis.py & packages_info_1.json

---
+++ dockerPull/Analysis/packages_info/package_info_analysis.py
@@ -0,0 +1,14 @@
+import json
+
+# 加载 JSON 文件
+with open('package_info_data/packages_info_1.json', 'r', encoding='utf-8') as f:
+    data = json.load(f)
+
+
+
+print(len(data))
+# 遍历并打印所有的 key 和对应的 values
+# for key, versions in data.items():
+#     print(f"Package: {key}")
+#     print(f"Versions: {', '.join(versions)}")
+#     print("="*40)
